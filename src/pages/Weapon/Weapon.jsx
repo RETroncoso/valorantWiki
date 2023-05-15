fd45220a01a81e8ddc21a85f5be0195e11de3dd1
Rodrigo Troncoso <rodrigo-rx@hotmail.com>
2023-05-14 21:18:23 -0300

    Weapon dynamic. Damage to do

--- src/pages/Weapon/Weapon.jsx
+++ src/pages/Weapon/Weapon.jsx
@@ -1,12 +1,56 @@
 import React from 'react'
-import { useLocation, useParams } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
+import { SectionWrapper } from '../../components/SectionWrapper/SectionWrapper'
+import { CoreDataContainer, DataContainer, DataRow, ImgContainer, MiscDataContainer, RowData, RowTitle } from './WeaponStyles'
 
 const Weapon = () => {
-    const {bullets, damage, description, equipSpeed, fireRate, img, name, penetration, price, reloadSpeed, shotType, type} = useLocation().state
+    const {bullets, damage, description, equipSpeed, fireRate, img, name, penetration, price, reloadSpeed, shotType, type} = useLocation().state;
 
+  console.log(damage);
 
     return (
-      <div>{name}</div>
+      <SectionWrapper>
+        <CoreDataContainer>
+          <ImgContainer>
+            <img src={img} alt={name} />
+          </ImgContainer>
+          <DataContainer>
+            <h2>{name}</h2>
+            <h3>{type}</h3>
+            <p>{description}</p>
+          </DataContainer>
+        </CoreDataContainer>
+        <MiscDataContainer>
+          <DataRow>
+            <RowTitle>Precio</RowTitle>
+            <RowData>{price} créditos</RowData>
+          </DataRow>
+          <DataRow>
+            <RowTitle>Cargador</RowTitle>
+            <RowData>{bullets} balas</RowData>
+          </DataRow>
+          <DataRow>
+            <RowTitle>Velocidad de equipamiento</RowTitle>
+            <RowData>{equipSpeed} seg</RowData>
+          </DataRow>
+          <DataRow>
+            <RowTitle>Cadencia de disparo</RowTitle>
+            <RowData>{fireRate} balas/seg</RowData>
+          </DataRow>
+          <DataRow>
+            <RowTitle>Penetracion</RowTitle>
+            <RowData>{penetration}</RowData>
+          </DataRow>
+          <DataRow>
+            <RowTitle>Velocidad de recarga</RowTitle>
+            <RowData>{reloadSpeed} segs</RowData>
+          </DataRow>
+          <DataRow>
+            <RowTitle>Tipo de Disparo</RowTitle>
+            <RowData>{shotType}</RowData>
+          </DataRow>
+        </MiscDataContainer>
+      </SectionWrapper>
     )
 }
 
---
+++ src/pages/Weapon/WeaponStyles.js
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+
+export const CoreDataContainer = styled.div`
+	display: flex;
+	width: 90%;
+	height: 100%;
+	justify-content: space-around;
+	align-items: center;
+`;
+
+export const ImgContainer = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	height: 100%;
+	width: 40%;
+	img {
+		width: 100%;
+		filter: drop-shadow(0px 0px 14px #848484);
+	}
+`;
+
+export const DataContainer = styled.div`
+	height: 100%;
+	width: 40%;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-around;
+	gap: 30px;
+	h2 {
+		font-size: 4rem;
+	}
+	h3 {
+		font-size: 2rem;
+	}
+	p {
+		font-size: 1.25rem;
+	}
+`;
+
+export const MiscDataContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	width: 60%;
+`;
+
+export const DataRow = styled.div`
+	display: flex;
+	justify-content: space-between;
+	border: 1px solid white;
+	padding: 10px 30px;
+`;
+
+export const RowTitle = styled.span`
+	font-weight: 800;
+`;
+
+export const RowData = styled.span``;
